bbe66d119c93794a2ca0fe85a3e2ee481b489568
Deividas Kondratjevas <deividaskondgmail.com>
2025-04-06 14:58:41 +0200

    working on django

--- sales/models.py
+++ sales/models.py
@@ -4,16 +4,32 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30, error_messages='huppla', help_text='max 30 letters dummy')
     last_name = models.CharField(max_length=30)
     newsletter_abo = models.BooleanField(default=True)
     email_address = models.EmailField(blank=True, default="")
     account = models.FloatField(blank=True, null=True)
+    slug = models.SlugField(blank=True, default='')
+
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural ='Customers'
+        ordering=['first_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def save(self):
+        self.account = 651681
+        return super().save()
 
 
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
 
 class Bill(models.Model):
     total_amount = models.FloatField()
@@ -29,4 +45,8 @@ class Producttype(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     type_name = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return f"{self.type_name}"
 
