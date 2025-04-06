6d9c9545f309810b8e6fa40b766bdb81c3301c26
Deividas Kondratjevas <deividaskondgmail.com>
2025-04-06 19:54:13 +0200

    django

--- sales/models.py
+++ sales/models.py
@@ -4,26 +4,17 @@ from django.db import models
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=30, error_messages='huppla', help_text='max 30 letters dummy')
+    first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     newsletter_abo = models.BooleanField(default=True)
-    email_address = models.EmailField(blank=True, default="")
+    email_address = models.CharField(blank=True, max_length=30)
     account = models.FloatField(blank=True, null=True)
-    slug = models.SlugField(blank=True, default='')
 
-    class Meta:
-        verbose_name = 'Customer'
-        verbose_name_plural ='Customers'
-        ordering=['first_name']
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
-    def save(self):
-        self.account = 651681
-        return super().save()
-
-
+  
 class Product(models.Model):
     name = models.CharField(max_length=30)
     price = models.FloatField()
@@ -44,7 +35,7 @@ class Order(models.Model):
 class Producttype(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    type_name = models.CharField(max_length=30)
+    type_name = models.CharField(max_length=300)
 
 
     def __str__(self):
